b9b6d33dc4ac1b63633f272e0c97f098b8d5538d
Gift <giftjeremiah001@gmail.com>
2023-07-23 13:25:39 +0100

    refact: refactored some redundant code

--- site_scraper/utils.py
+++ site_scraper/utils.py
@@ -2,14 +2,6 @@ import requests
 import difflib
 
 def suggest_website_name(site_name):
-    """Generate website name suggestions by combining words.
-
-    Args:
-    site_name: The name of the website.
-
-    Returns:
-    A list of website name suggestions.
-    """
     words = site_name.split()
     combinations = []
     for i in range(1, len(words) + 1):
@@ -17,21 +9,15 @@ def suggest_website_name(site_name):
     return combinations
 
 def get_website_url(site_name):
-    """Gets the website URL of the given site name.
 
-    Args:
-    site_name: The name of the website.
-
-    Returns:
-    The website URL or a list of suggestions if the URL is not found.
-    """
     try:
         site_name = site_name.strip().replace(' ', '')
         url = 'https://www.{}.com'.format(site_name)
         response = requests.get(url)
-        response.raise_for_status()  # Raise an exception if response status is not 2xx
+        response.raise_for_status()  # Raise an exception if response status is not 
         return response.url
     except requests.exceptions.RequestException:
+        #give suggestions if you can't find the url
         suggestions = suggest_website_name(site_name)
         closest_matches = difflib.get_close_matches(site_name, suggestions, n=3, cutoff=0.6)
         suggestion_str = ', '.join(closest_matches)
